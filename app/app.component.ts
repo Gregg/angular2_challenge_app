c97bdbcffa834ff011e8b669122976b71ab295f7
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 18:52:09 -0400

    C7T3-End

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,8 @@ import { RacesComponent } from './races.component';
   selector: 'racing-app',
   template: `<h1>{{heading}}</h1>
 <my-races></my-races>
-`
+`,
+  directives: [RacesComponent]
 })
 export class AppComponent {
   heading = "Ultra Racing Schedule"
