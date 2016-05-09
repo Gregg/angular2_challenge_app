9a4e9f6b31161ecbc237d5c578a0d94af85b00b7
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 15:38:46 -0400

    C2T1-End

--- app/main.ts
+++ app/main.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'racing-app',
   template: `<h1>{{heading}}</h1>
-  <h2></h2>
-  <p></p>
-  <p></p>
+  <h2>{{race.name}}</h2>
+  <p>{{race.date}}</p>
+  <p>{{race.about}}</p>
   `
 })
 export class AppComponent {
