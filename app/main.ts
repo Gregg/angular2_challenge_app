4d27661a1eeadb920abf8685a9ea839a8f416b62
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 15:34:15 -0400

    C2T1-Start

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'racing-app',
-  template: '<h1>{{heading}}</h1>'
+  template: `<h1>{{heading}}</h1>
+  <h2></h2>
+  <p></p>
+  <p></p>
+  `
 })
 export class AppComponent {
   heading = "Ultra Racing Schedule"
+  race = {
+    "id": 1,
+    "name": "Daytona Thunderdome",
+    "date": new Date('2512-01-04T14:00:00'),
+    "about": "Race through the ruins of an ancient Florida battle arena."
+    "entryFee": 3200
+  }
 }
 
 bootstrap(AppComponent);
