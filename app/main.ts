1c761e6d73d468b56ec6e03f938a787f2b1621fc
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 15:52:34 -0400

    C3T1-Start

--- app/main.ts
+++ app/main.ts
@@ -4,20 +4,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'racing-app',
   template: `<h1>{{heading}}</h1>
-  <h2>{{race.name}}</h2>
-  <p>{{race.date}}</p>
-  <p>{{race.about}}</p>
+<ul>
+  <li>
+    <h2>{{race.name}}</h2>
+    <p>{{race.date}}</p>
+    <p>{{race.about}}</p>
+  </li>
+</ul>
   `
 })
 export class AppComponent {
   heading = "Ultra Racing Schedule"
-  race = {
+  races = [{
     "id": 1,
     "name": "Daytona Thunderdome",
     "date": new Date('2512-01-04T14:00:00'),
-    "about": "Race through the ruins of an ancient Florida battle arena."
+    "about": "Race through the ruins of an ancient Florida battle arena.",
     "entryFee": 3200
+  }, {
+    "id": 2,
+    "name": "San Francisco Ruins",
+    "date": new Date('2512-07-03T20:00:00'),
+    "about": "Drift down the streets of a city almost sunk under the ocean.",
+    "entryFee": 4700
+  }, {
+    "id": 3,
+    "name": "New York City Skyline",
+    "date": new Date('2512-07-12T21:00:00'),
+    "about": "Fly between buildings in the electronic sky.",
+    "entryFee": 4300
   }
+
 }
 
 bootstrap(AppComponent);
