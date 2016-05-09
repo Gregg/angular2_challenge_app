9cba36b17f2c17273ccb40e9abe5693b726e7c49
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 16:14:38 -0400

    C4T1-Start

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,8 @@ import { Component } from '@angular/core';
     <h2>{{race.name}}</h2>
     <p>{{race.date}}</p>
     <p>{{race.about}}</p>
+    <button>Enter Race</button>
+    <h3>Already Racing</h3>
   </li>
 </ul>
   `
@@ -20,19 +22,22 @@ export class AppComponent {
     "name": "Daytona Thunderdome",
     "date": new Date('2512-01-04T14:00:00'),
     "about": "Race through the ruins of an ancient Florida battle arena.",
-    "entryFee": 3200
+    "entryFee": 3200,
+    "isRacing": false
   }, {
     "id": 2,
     "name": "San Francisco Ruins",
     "date": new Date('2512-07-03T20:00:00'),
     "about": "Drift down the streets of a city almost sunk under the ocean.",
-    "entryFee": 4700
+    "entryFee": 4700,
+    "isRacing": true
   }, {
     "id": 3,
     "name": "New York City Skyline",
     "date": new Date('2512-07-12T21:00:00'),
     "about": "Fly between buildings in the electronic sky.",
-    "entryFee": 4300
+    "entryFee": 4300,
+    "isRacing": false
   }
 
 }
