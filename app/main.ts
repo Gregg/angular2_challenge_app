ab373326a67e2dacde78c1b608de0e79b75ac414
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 14:43:50 -0400

    C1T2-End

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { Component } from '@angular/core';
 
+@Component({
+  selector: 'racing-app',
+  template: '<h1>{{heading}}</h1>'
+})
 export class AppComponent {
   heading = "Ultra Racing Schedule"
 }
