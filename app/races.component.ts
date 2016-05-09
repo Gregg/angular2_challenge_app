5c4aff796a74889f4ffc9e5ad1fdcedb51d80ce3
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 19:49:53 -0400

    C8T2 - End

--- app/races.component.ts
+++ app/races.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-races',
-  templateUrl: 'app/races.component.html'
+  templateUrl: 'app/races.component.html',
+  styleUrls:['app/races.component.css']
 })
 export class RacesComponent {
   heading = "Ultra Racing Schedule"
