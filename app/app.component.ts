a4cbd9f4a856fe71b6e8cc7945b00d3cb5a8c3ab
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 14:13:25 -0400

    Setting up for first challenge

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
-})
-export class AppComponent { }
+export class AppComponent {
+  title = "Ultra Racing Schedule"
+}
