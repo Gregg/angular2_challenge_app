e74b2e170793bafbb0790811ec04af943808255a
Gregg Pollack <Gregg@EnvyLabs.com>
2016-05-09 19:53:10 -0400

    C9T1 - Start

--- app/races.component.ts
+++ app/races.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Race } from './race';
+import { RACES } from './mocks';
 
 @Component({
   selector: 'my-races',
@@ -8,28 +10,8 @@ import { Component } from '@angular/core';
 export class RacesComponent {
   heading = "Ultra Racing Schedule"
   cash = 10000;
-  races = [{
-    "id": 1,
-    "name": "Daytona Thunderdome",
-    "date": new Date('2512-01-04T14:00:00'),
-    "about": "Race through the ruins of an ancient Florida battle arena.",
-    "entryFee": 3200,
-    "isRacing": false
-  }, {
-    "id": 2,
-    "name": "San Francisco Ruins",
-    "date": new Date('2512-07-03T20:00:00'),
-    "about": "Drift down the streets of a city almost sunk under the ocean.",
-    "entryFee": 4700,
-    "isRacing": true
-  }, {
-    "id": 3,
-    "name": "New York City Skyline",
-    "date": new Date('2512-07-12T21:00:00'),
-    "about": "Fly between buildings in the electronic sky.",
-    "entryFee": 4300,
-    "isRacing": true
-  }];
+  races: Race[];
+
 
   totalCost() {
     let sum = 0;
